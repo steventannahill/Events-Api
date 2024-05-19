4dd7bb4ddc5316ff93ce60ca259a09c1363f6f64
Steve <stevedtannahill@gmail.com>
2024-05-19 04:01:43 +0000

    DB events table created, endpoint for creating an event and getting all events

---
+++ db/db.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+  "fmt"
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+  var err error
+  
+  DB, err = sql.Open("sqlite3", "api.db")
+  
+  if err != nil {
+    panic("Could not connect to database!")
+  }
+  
+  DB.SetMaxOpenConns(10)
+  DB.SetMaxIdleConns(5)
+  
+  fmt.Println("init DB:",DB)
+  createTables()
+  
+  
+}
+
+func createTables() {
+  createEventsTable := `
+  CREATE TABLE IF NOT EXISTS events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    location TEXT NOT NULL,
+    dateTime DATETIME NOT NULL,
+    user_id INTEGER
+  )
+  
+  `
+  
+  _, err := DB.Exec(createEventsTable)
+  
+  if err != nil {
+    panic("Could not create events table.")
+  }
+}
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "github.com/gin-gonic/gin"
+  "example.com/events-api/models"
+  "example.com/events-api/db"
+)
 
 func main() {
-	fmt.Println("Hello, Go in CodeSandbox!")
+  
+  db.InitDB()
+  fmt.Println("global DB", db.DB)
+  
+  server := gin.Default()
+  
+  server.GET("/events", getEvents)
+  server.POST("/events", createEvent)
+  
+  server.Run(":8080") //localhost:8080
+  
+  
+}
+
+func getEvents(context *gin.Context){
+  events, err := models.GetAllEvents()
+  if err != nil {
+    context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+    return 
+  }
+  context.JSON(http.StatusOK, events)
+  
+}
+
+func createEvent(context *gin.Context) {
+  var event models.Event
+  err := context.ShouldBindJSON(&event)
+  if err != nil {
+    context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+    return
+  }
+  
+  event.UserID = 111
+  
+  err = event.Save()
+  if err != nil {
+    context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event. Try again later."})
+    return 
+  }
+  
+  
+  context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
